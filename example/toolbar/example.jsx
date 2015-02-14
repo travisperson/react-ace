8332e968da5458f7d398765ee59d68094b724529
Travis Person <travis.person@gmail.com>
2015-02-13 23:35:08 -0800

    Added mode and changed some defaults, as well as added more options

--- example/toolbar/example.jsx
+++ example/toolbar/example.jsx
@@ -3,21 +3,33 @@ var AceEditor  = require('../../src/ace.jsx');
 
 var ToolBar = React.createClass({
   handleChange: function() {
-    this.props.onToolBarChange(
-      this.refs.theme.getDOMNode().value
-    )
+    this.props.onToolBarChange({
+      theme: this.refs.theme.getDOMNode().value,
+      mode: this.refs.mode.getDOMNode().value
+    })
   },
   render: function() {
-    var options = [];
+    var themes = [];
     this.props.themes.forEach(function(theme) {
       var name = theme.charAt(0).toUpperCase() + theme.slice(1);
-      options.push(<option value={theme}>{name}</option>)
+      themes.push(<option value={theme}>{name}</option>)
+    }.bind(this))
+
+    var modes = [];
+    this.props.modes.forEach(function(theme) {
+      var name = theme.charAt(0).toUpperCase() + theme.slice(1);
+      modes.push(<option value={theme}>{name}</option>)
     }.bind(this))
 
     return (
-      <select ref="theme" onChange={this.handleChange}>
-        {options}
-      </select>
+      <div>
+        <select ref="theme" onChange={this.handleChange}>
+          {themes}
+        </select>
+        <select ref="mode" onChange={this.handleChange}>
+          {modes}
+        </select>
+      </div>
     )
   }
 })
@@ -25,18 +37,47 @@ var ToolBar = React.createClass({
 var ToolBarEditor = React.createClass({
   getInitialState: function() {
     return ({
-      theme: "github"
+      theme: "github",
+      mode: "javascript",
     });
   },
   getDefaultProps: function () {
     return ({
-      themes: ["github", "monokai"]
+      themes: [
+        "monokai",
+        "github",
+        "tomorrow",
+        "kuroir",
+        "twilight",
+        "xcode",
+        "textmate",
+        "terminal",
+        "solarized_dark",
+        "solarized_light"
+      ],
+      modes: [
+        "javascript",
+        "java",
+        "c_cpp",
+        "php",
+        "python",
+        "xml",
+        "ruby",
+        "sass",
+        "markdown",
+        "mysql",
+        "json",
+        "html",
+        "handlebars",
+        "golang",
+        "csharp",
+        "coffee",
+        "css"
+      ]
     });
   },
-  handleToolBarChange: function(theme) {
-    this.setState({
-      theme: theme
-    })
+  handleToolBarChange: function(ob) {
+    this.setState(ob)
   },
   handlePatch: function(patch) {
     console.log("Got Patch")
@@ -45,8 +86,8 @@ var ToolBarEditor = React.createClass({
   render: function () {
     return (
       <div>
-        <ToolBar onToolBarChange={this.handleToolBarChange} themes={this.props.themes}/>
-        <AceEditor mode="javascript" theme={this.state.theme} name="toolbar-editor" height="6em" onChange={this.handlePatch}/>
+        <ToolBar onToolBarChange={this.handleToolBarChange} themes={this.props.themes} modes={this.props.modes}/>
+        <AceEditor theme={this.state.theme} mode={this.state.mode} name="toolbar-editor" height="6em" onChange={this.handlePatch}/>
       </div>
     )
   }
